2be51182e47aa8896db03616b85a6c2f7e2b3979
Kate Miháliková <kate@katemihalikova.cz>
2025-06-06 09:08:22 +0200

    Add style, center, zoom to map

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css';
 
-import { config, Map, type MapOptions } from '@maptiler/sdk';
+import { config, Map, type MapOptions, MapStyle } from '@maptiler/sdk';
 
 import '@maptiler/sdk/dist/maptiler-sdk.css';
 
@@ -8,6 +8,9 @@ config.apiKey = 'clZrmP7UgVs1sKWo5A8V';
 
 const options: MapOptions = {
   container: document.getElementById('map') as HTMLElement,
+  style: MapStyle.OUTDOOR,
+  center: [15.1707594, 50.7259572],
+  zoom: 3,
 };
 
 const map = new Map(options);
